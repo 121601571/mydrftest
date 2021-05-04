25e46c7a1288bfb536cf4fbc9c00c0c187a121c3
121601571 <48176182+121601571@users.noreply.github.com>
2021-05-04 14:16:38 +0800

    updated version

--- myproj/myapp/cbv2.py
+++ myproj/myapp/cbv2.py
@@ -2,12 +2,14 @@ from .models import Snippet
 from .serializers import SnippetSerializer
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework import permissions
 
 class SnippetList(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
                   generics.GenericAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
